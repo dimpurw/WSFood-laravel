a56cfe4770eb49260d7752bc4785a94e16caa737
DimasPratamaP <terminatorid2@gmail.com>
2020-09-14 22:06:36 +0700

    add card view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,50 +64,116 @@
     <!-- end jumbotron -->
 
     <!-- description wsfood -->
-    <p class="text-md-center description">wsfood is a web about various foods, <br> drinks and snacks, with the addition of this website can make it easier for us to find food</p>
-    <div class="container">
-      <div class="row">
-        <div class="col text-center">
-          <button class="btn btn-primary">Centered button</button>
+    <div class="bg-view">
+      <p class="text-md-center description">wsfood is a web about various foods, <br> drinks and snacks, with the addition of this website can make it easier for us to find food</p>
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <button class="btn btn-primary">Centered button</button>
+          </div>
         </div>
       </div>
     </div>
     <!-- end description -->
 
     <!-- card view -->
-    <div class="card-deck card-view">
-      <div class="card">
-        <img src="{{ URL::to('/assets/images/Chicken_noodle_soup.jpg')}}" class="card-img-top h-50" alt="jumbotron-image">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div class="card-deck card-view">
+        <div class="card">
+          <img src="{{ URL::to('/assets/images/Chicken_noodle_soup.jpg')}}" class="card-img-top h-50" alt="jumbotron-image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
         </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+        <div class="card">
+          <img src="{{ URL::to('/assets/images/fried-chicken.jpeg')}}" class="card-img-top h-50" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img src="{{ URL::to('/assets/images/high-angle.jpg')}}" class="card-img-top h-50" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
         </div>
       </div>
-      <div class="card">
-        <img src="{{ URL::to('/assets/images/fried-chicken.jpeg')}}" class="card-img-top h-50" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <div class="card-deck mt-3">
+        <div class="card">
+          <img src="{{ URL::to('/assets/images/cocktail.jpg')}}" class="card-img-top h-50" alt="jumbotron-image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
         </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+        <div class="card">
+          <img src="{{ URL::to('/assets/images/pineaplle.jpg')}}" class="card-img-top h-50" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img src="{{ URL::to('/assets/images/pink_shirley.jpg')}}" class="card-img-top h-50" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
         </div>
       </div>
-      <div class="card">
-        <img src="{{ URL::to('/assets/images/high-angle.jpg')}}" class="card-img-top h-50" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <div class="card-deck mt-3">
+        <div class="card">
+          <img src="{{ URL::to('/assets/images/maggi.jpg')}}" class="card-img-top h-50" alt="jumbotron-image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
         </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+        <div class="card">
+          <img src="{{ URL::to('/assets/images/snack-cheese.jpg')}}" class="card-img-top h-50" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img src="{{ URL::to('/assets/images/snack-mix.jpg')}}" class="card-img-top h-50" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- end card view -->
+    <!-- end card -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
