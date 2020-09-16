d86cde67443e7980d14d6f72fcf28fd5ad412a77
DimasPratamaP <terminatorid2@gmail.com>
2020-09-16 10:53:44 +0700

    update page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,7 +58,7 @@
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
           <h1 class="display-4 text-light font-weight-bold mt-5">Fluid jumbotron</h1>
-          <p class="lead text-light">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          <p class="lead text-light" id="write-text"></p>
         </div>
     </div>
     <!-- end jumbotron -->
@@ -174,8 +174,16 @@
         </div>
       </div>
     <!-- end card -->
+
+    <!-- page our visitor -->
+    <div class="bg-view mt-5">
+      <p class="text-md-center description"><span>our visitor</span><br> below is the opinion of our web visitors about the WSFood web</p>
+    </div>
+    <!-- end page our visitor -->
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
     </body>
 </html>
