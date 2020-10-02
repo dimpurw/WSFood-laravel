e25f09c3ccc50ad1b0e7c02de9846fc573f8d38d
DimasPratamaP <terminatorid2@gmail.com>
2020-10-02 09:07:00 +0700

    js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,14 @@
-const text = "This is a modified jumbotron that occupies the entire horizontal space of its parent.";
+const text =
+    "This is a modified jumbotron that occupies the entire horizontal space of its parent.";
 
 let index = 0;
 
-function writeText(){
-    document.getElementById("write-text").innerText = text.slice(0,index);
+function writeText() {
+    document.getElementById("write-text").innerText = text.slice(0, index);
 
     index++;
 
-    if(index > text.length){
+    if (index > text.length) {
         index = 0;
     }
 }
